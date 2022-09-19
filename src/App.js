01b0986313f25fad5d6fcfec01ce948cef5dee20
bbd <dolgkozly@gmail.com>
2022-09-19 12:17:23 +0300

    Add ViewPortParams

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import Calendar from './components/Calendar';
+import ViewPortParams from './components/ViewPortParams';
 
-const App = () => <Calendar />;
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      isVisible: true,
+    };
+  }
+  render () {
+    const { isVisible } = this.state;
+
+    return (
+      <>
+        <button
+          onClick={() => {
+            this.setState({ isVisible: !isVisible });
+          }}
+        >
+          Switch
+        </button>
+        {isVisible && <ViewPortParams />}
+      </>
+    );
+  }
+}
 
 export default App;
