55bc09b57aa3d16a3ae4d01abada955b977be9dc
bbd <dolgkozly@gmail.com>
2022-09-23 22:37:40 +0300

    Add prop children: NamedList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import LoginForm from './components/LoginForm';
-import UsersLoader from './components/UsersLoader';
+import React from 'react';
 
-const App = () => <LoginForm />;
+const ListItem = props => {
+  const { children } = props;
+  return <div>{children}</div>;
+};
+
+const NamedList = props => {
+  const { name, children } = props;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>{children}</ul>
+    </div>
+  );
+};
+
+const App = () => (
+  <NamedList name='Odd numbers'>
+    <li>1</li>
+    <li>3</li>
+    <li>5</li>
+  </NamedList>
+);
 
 export default App;
